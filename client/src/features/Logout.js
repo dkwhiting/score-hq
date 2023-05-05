c293e61359e929b3ef7f86bca11fb7c30096fc7e
Dallin Whiting <dallinkwhiting@gmail.com>
2023-05-05 10:55:36 -0600

    Separated components

---
+++ client/src/features/Logout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Text, View, Button } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux';
+import { removeData } from '../utils';
+import { setUser } from '../app/userSlice';
+
+
+
+const Logout = () => {
+  const dispatch = useDispatch()
+  return (
+    <View style={{width: '100%', height: 65, backgroundColor: '#CCC', marginTop: 'auto'}}>
+      <Button title="Logout" onPress={()=> {
+        removeData('currentUser');
+        dispatch(setUser(null));
+
+        }} />
+    </View>
+  )
+}
+
+export default Logout
