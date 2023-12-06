78725f40e6479b3559d617a35115ae432697dec9
Dallin Whiting <dallinkwhiting@gmail.com>
2023-12-06 11:46:57 -0700

    Upgraded to react-router-dom v6.4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,65 +1,19 @@
-import Login from './components/Login';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useLayoutEffect, useState} from 'react';
-import { setUser } from './app/userSlice';
-import { useGetAllGamesQuery } from './app/shopAPI';
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer'
-import Header from './components/Header';
-import { setGames } from './app/gamesSlice';
+import React from 'react'
+import {
+  createBrowserRouter,
+  Link,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import Root from './Root';
 
-const Main = () => {
-  const [darkMode, setDarkMode] = useState(false)
-  const user = useSelector(state => state.user.currentUser)
-  const games = useSelector(state => state.user.games)
-  const {data, isLoading, isError} = useGetAllGamesQuery(user?.id)
-  const dispatch = useDispatch()
-  
-  
-  const initializeUser = () => {
-    const currentUser = localStorage.getItem('currentUser')
-    dispatch(setUser(JSON.parse(currentUser)))
-    const token = localStorage.getItem('token')
-  }
-  
-  const initializeGames = () => {
-    
-    
+// 3️⃣ Router singleton created
+const router = createBrowserRouter([
+  { path: "*", Component: Root },
+]);
 
-  }
-  
-  useEffect(()=>{
-    if (!user){
-      initializeUser()
-    } else {
-      dispatch(setGames(data))
-      console.log(data)
-    }
-  }, [])
-
-  useEffect(()=>{
-    //initialize games upon user change
-    if (user){
-      dispatch(setGames(data))
-    }
-  },[user])
-
-  return (
-    <div className={`h-full ${darkMode ? 'dark' : ''} transition-all duration-300`}>
-      
-      <div className="flex flex-col h-full bg-white dark:bg-gray-800 text-black dark:text-white transition-all duration-300">
-        {
-          !user
-          ? <Login/>
-          : <>
-              <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
-              <Dashboard />
-              <Footer />
-            </>
-      }
-      </div>
-    </div>
-  );
+// 4️⃣ RouterProvider added
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default Main
